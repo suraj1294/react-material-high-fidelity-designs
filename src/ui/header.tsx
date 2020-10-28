155154d8ea28b80987dc5c3ecc26317f2b0fda3a
Suraj Patil <suraj.patil@frontdoorhome.com>
2020-10-28 10:23:07 +0530

    complete header part

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -11,11 +11,17 @@ import {
   Menu,
   MenuItemProps,
 } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Tabs from "@material-ui/core/Tabs";
-
 import Tab, { TabProps } from "@material-ui/core/Tab";
-
 import logo from "../assets/logo.svg";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import List from "@material-ui/core/List";
+import ListItem, { ListItemProps } from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 const LinkTab: React.ComponentType<
   TabProps & LinkProps
@@ -28,6 +34,10 @@ const LinkButton: React.ComponentType<
 const LinkMenuItem: React.ComponentType<
   LinkProps & MenuItemProps
 > = MenuItem as React.ComponentType<LinkProps & MenuItemProps>;
+
+const LinkListItem: React.ComponentType<
+  LinkProps & ListItemProps
+> = ListItem as React.ComponentType<LinkProps & ListItemProps>;
 
 interface Props {
   /**
@@ -58,9 +68,21 @@ const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
     marginBottom: "3em",
+    [theme.breakpoints.down("md")]: {
+      marginBottom: "2em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginBottom: "1.2em",
+    },
   },
   logo: {
-    height: "7em",
+    height: "8em",
+    [theme.breakpoints.down("md")]: {
+      height: "7em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      height: "5.5em",
+    },
   },
   logoContainer: {
     padding: 0,
@@ -76,7 +98,7 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 10,
     marginLeft: "24px",
   },
-  button: {
+  buttonEstimate: {
     ...theme.typography.estimate,
     marginRight: "24px",
     marginLeft: "50px",
@@ -94,39 +116,76 @@ const useStyles = makeStyles((theme) => ({
       opacity: 1,
     },
   },
-  menuItemSelected: {
+  drawerIconContainer: {
+    marginLeft: "auto",
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
+  drawerMenuIcon: {
+    height: "50px",
+    width: "50px",
+  },
+  drawer: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  drawerItem: {
+    ...theme.typography.tab,
+    color: "white",
+    opacity: 0.7,
+  },
+  drawerItemEstimate: {
+    ...theme.typography.estimate,
     backgroundColor: theme.palette.secondary.main,
+    borderRadius: 0,
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.main,
+      opacity: 0.7,
+    },
+  },
+  selectedDrawerItem: {
+    ...theme.typography.tab,
+    color: "white",
     opacity: 1,
+  },
+  appbar: {
+    zIndex: theme.zIndex.modal + 1,
   },
 }));
 
-const routesWithIndex = [
-  { path: "/", index: 0 },
-  { path: "/services", index: 1 },
-  { path: "/services", index: 1 },
-  { path: "/customSoftware", index: 1 },
-  { path: "/mobileApps", index: 1 },
-  { path: "/websites", index: 1 },
-  { path: "/revolution", index: 2 },
-  { path: "/about", index: 3 },
-  { path: "/contact", index: 4 },
+const routes = [
+  { name: "Home", path: "/", index: 0 },
+  { name: "Services", path: "/services", index: 1, haveSubMenu: true },
+  { name: "The Revolution", path: "/revolution", index: 2 },
+  { name: "About Us", path: "/about", index: 3 },
+  { name: "Contact Us", path: "/contact", index: 4 },
+];
+
+const menuOptions = [
+  { name: "Services", path: "/services", index: 1 },
+  { name: "Custom Software Development", path: "/customSoftware", index: 1 },
+  { name: "Mobile App development", path: "/mobileApps", index: 1 },
+  { name: "Website Development", path: "/websites", index: 1 },
 ];
 
 const getRouteIndex = (path: string) => {
-  const route = routesWithIndex.find((route) => route.path === path);
+  const route = [...routes, ...menuOptions].find(
+    (route) => route.path === path
+  );
   return route?.index;
 };
 
-const menuOptions = [
-  { name: "Services", path: "/services" },
-  { name: "Custom Software Development", path: "/customSoftware" },
-  { name: "Mobile App development", path: "/mobileApps" },
-  { name: "Website Development", path: "/websites" },
-];
-
 const Header: FC<{}> = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+  const iOS =
+    (process as any).browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  const [openDrawer, setOpenDrawer] = useState(false);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [openMenu, setOpenMenu] = React.useState(false);
+
   const [selectedTabIndex, setTabIndex] = useState(
     getRouteIndex(window.location.pathname)
   );
@@ -137,15 +196,120 @@ const Header: FC<{}> = () => {
 
   const handleClick = (event: any) => {
     setAnchorEl(event.currentTarget);
+    setOpenMenu(true);
   };
 
   const handleClose = () => {
     setAnchorEl(null);
+    setOpenMenu(false);
   };
+
+  const tabs = (
+    <Tabs
+      value={selectedTabIndex}
+      indicatorColor="primary"
+      className={classes.tabContainer}
+      onChange={handleTabChange}
+    >
+      {routes.map((route) =>
+        !route.haveSubMenu ? (
+          <LinkTab
+            key={route.index}
+            className={classes.tab}
+            label={route.name}
+            component={Link}
+            to={route.path}
+          />
+        ) : (
+          <LinkTab
+            key={route.index}
+            aria-controls={anchorEl ? "simple-menu" : undefined}
+            aria-haspopup={openMenu ? "true" : undefined}
+            className={classes.tab}
+            label={route.name}
+            component={Link}
+            to={route.path}
+            onMouseOver={handleClick}
+          />
+        )
+      )}
+
+      <LinkButton
+        variant="contained"
+        color="secondary"
+        className={classes.buttonEstimate}
+        component={Link}
+        to="/estimate"
+      >
+        Free Estimate
+      </LinkButton>
+    </Tabs>
+  );
+
+  const drawer = (
+    <>
+      <SwipeableDrawer
+        open={openDrawer}
+        onOpen={() => setOpenDrawer(true)}
+        onClose={() => setOpenDrawer(false)}
+        disableBackdropTransition={!iOS}
+        disableDiscovery={iOS}
+        classes={{ paper: classes.drawer }}
+      >
+        <div className={classes.toolbarMargin}></div>
+        <List disablePadding>
+          {routes.map((route) => (
+            <LinkListItem
+              key={`${route.name}${route.index}`}
+              button
+              divider
+              component={Link}
+              to={route.path}
+              selected={
+                route.path === window.location.pathname ||
+                getRouteIndex(route.path) === selectedTabIndex
+              }
+              onClick={() => {
+                setOpenDrawer(false);
+                setTabIndex(route.index);
+              }}
+              classes={{ selected: classes.selectedDrawerItem }}
+              className={classes.drawerItem}
+            >
+              <ListItemText disableTypography>{route.name}</ListItemText>
+            </LinkListItem>
+          ))}
+
+          <LinkListItem
+            button
+            component={Link}
+            to="/estimate"
+            selected={"/estimate" === window.location.pathname}
+            onClick={() => {
+              setOpenDrawer(false);
+              setTabIndex(5);
+            }}
+            className={classes.drawerItemEstimate}
+            classes={{ selected: classes.selectedDrawerItem }}
+          >
+            <ListItemText disableTypography>Free Estimate</ListItemText>
+          </LinkListItem>
+        </List>
+      </SwipeableDrawer>
+      <IconButton
+        disableRipple
+        className={classes.drawerIconContainer}
+        onClick={() => setOpenDrawer(!openDrawer)}
+      >
+        <MenuIcon className={classes.drawerMenuIcon} />
+      </IconButton>
+    </>
+  );
+
   return (
     <>
       <ElevationScroll>
-        <AppBar position="fixed">
+        <AppBar position="fixed" className={classes.appbar}>
           <Toolbar disableGutters>
             <LinkButton
               component={Link}
@@ -156,55 +320,16 @@ const Header: FC<{}> = () => {
             >
               <img src={logo} alt="Company logo" className={classes.logo} />
             </LinkButton>
-            <Tabs
-              value={selectedTabIndex}
-              indicatorColor="primary"
-              className={classes.tabContainer}
-              onChange={handleTabChange}
-            >
-              <LinkTab
-                className={classes.tab}
-                label="Home"
-                component={Link}
-                to="/"
-              />
-
-              <LinkTab
-                aria-owns="simple-menu"
-                aria-haspopup="true"
-                className={classes.tab}
-                label="Services"
-                component={Link}
-                to="/services"
-                onMouseOver={handleClick}
-              />
-              <LinkTab
-                className={classes.tab}
-                label="The Revolution"
-                component={Link}
-                to="/revolution"
-              />
-              <LinkTab
-                className={classes.tab}
-                label="About Us"
-                component={Link}
-                to="/about"
-              />
-              <LinkTab
-                className={classes.tab}
-                label="Contact Us"
-                component={Link}
-                to="/contact"
-              />
-            </Tabs>
+            {matches ? drawer : tabs}
             <Menu
               id="simple-menu"
               anchorEl={anchorEl}
               keepMounted
-              open={Boolean(anchorEl)}
+              open={openMenu}
               onClose={handleClose}
               classes={{ paper: classes.menu }}
               elevation={0}
+              style={{ zIndex: 1302 }}
               MenuListProps={{ onMouseLeave: handleClose }}
             >
               {menuOptions.map((menuOption, index) => (
@@ -214,27 +339,17 @@ const Header: FC<{}> = () => {
                   to={menuOption.path}
                   classes={{
                     root: classes.menuItem,
-                    selected: classes.menuItemSelected,
                   }}
                   selected={menuOption.path === window.location.pathname}
                   onClick={() => {
-                    handleClose();
                     setTabIndex(1);
+                    handleClose();
                   }}
                 >
                   {menuOption.name}
                 </LinkMenuItem>
               ))}
             </Menu>
-            <LinkButton
-              variant="contained"
-              color="secondary"
-              className={classes.button}
-              component={Link}
-              to="/estimate"
-            >
-              Free Estimate
-            </LinkButton>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
